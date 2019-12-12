94f46fb2ffacc0d79f5f7195f8423036326a8ac0
Mariusz Dobrowolski <mariusz.dobrowolski@softwarehut.com>
2019-12-12 10:16:17 +0100

    update sum vector methods for benchmark

--- SIMDBenchmark/SIMDBenchmark/Program.cs
+++ SIMDBenchmark/SIMDBenchmark/Program.cs
@@ -14,261 +14,287 @@ namespace SIMDBenchmark
     {
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<MinValueBenchmark>();
+            //var summary = BenchmarkRunner.Run<MinValueBenchmark>();
             //var summary = BenchmarkRunner.Run<SumBenchmark>();
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+
             Console.ReadKey();
         }
     }
 
-    public class MinValueBenchmark
-    {
-        ushort[] source = Enumerable.Repeat(0, 2097152).Select(i => (ushort)new Random().Next(20, ushort.MaxValue - 1)).ToArray();
+    //public class MinValueBenchmark
+    //{
+    //    ushort[] source = Enumerable.Repeat(0, 2097152).Select(i => (ushort)new Random().Next(20, ushort.MaxValue - 1)).ToArray();
 
-        [Benchmark]
-        public unsafe ushort MinSIMD()
-        {
-            int volumen = source.Length;
+    //    [Benchmark]
+    //    public unsafe ushort MinSIMD()
+    //    {
+    //        int volumen = source.Length;
 
-            fixed (ushort* pSource = source)
-            {
-                do
-                {
-                    volumen = (int)Math.Ceiling(volumen / 8m);
+    //        fixed (ushort* pSource = source)
+    //        {
+    //            do
+    //            {
+    //                volumen = (int)Math.Ceiling(volumen / 8m);
 
-                    for (var i = 0; i < volumen; i++)
-                    {
-                        source[i] = Sse41.MinHorizontal(Sse2.LoadVector128(pSource + (i * 8))).GetElement(0);
-                    }
-                }
-                while (volumen > 8);
+    //                for (var i = 0; i < volumen; i++)
+    //                {
+    //                    source[i] = Sse41.MinHorizontal(Sse2.LoadVector128(pSource + (i * 8))).GetElement(0);
+    //                }
+    //            }
+    //            while (volumen > 8);
 
-                return Sse41.MinHorizontal(Sse2.LoadVector128(pSource)).GetElement(0);
-            }
-        }
+    //            return Sse41.MinHorizontal(Sse2.LoadVector128(pSource)).GetElement(0);
+    //        }
+    //    }
 
-        [Benchmark]
-        public unsafe ushort MinSIMDParalel()
-        {
-            int volumen = source.Length;
+    //    [Benchmark]
+    //    public unsafe ushort MinSIMDParalel()
+    //    {
+    //        int volumen = source.Length;
 
-            fixed (ushort* pSource = source)
-            {
-                var ops = new UnsafeOps();
-                ops.p = pSource;
-                ops.source = source;
+    //        fixed (ushort* pSource = source)
+    //        {
+    //            var ops = new UnsafeOps();
+    //            ops.p = pSource;
+    //            ops.source = source;
 
-                do
-                {
-                    volumen = (int)Math.Ceiling(volumen / 8m);
+    //            do
+    //            {
+    //                volumen = (int)Math.Ceiling(volumen / 8m);
 
-                    Parallel.For(0, volumen - 1, ops.Lambda);
+    //                Parallel.For(0, volumen - 1, ops.Lambda);
                     
-                }
-                while (volumen > 8);
+    //            }
+    //            while (volumen > 8);
 
-                return Sse41.MinHorizontal(Sse2.LoadVector128(pSource)).GetElement(0);
-            }
-        }
+    //            return Sse41.MinHorizontal(Sse2.LoadVector128(pSource)).GetElement(0);
+    //        }
+    //    }
 
-        private unsafe class UnsafeOps
-        {
-            public ushort* p;
-            public ushort[] source;
+    //    private unsafe class UnsafeOps
+    //    {
+    //        public ushort* p;
+    //        public ushort[] source;
 
-            public unsafe void Lambda(int shift)
-            {
-                source[shift] = Sse41.MinHorizontal(Sse2.LoadVector128(p + (shift * 8))).GetElement(0);
-            }
-        }
+    //        public unsafe void Lambda(int shift)
+    //        {
+    //            source[shift] = Sse41.MinHorizontal(Sse2.LoadVector128(p + (shift * 8))).GetElement(0);
+    //        }
+    //    }
 
-        [Benchmark]
-        public ushort MinLINQ()
-        {
+    //    [Benchmark]
+    //    public ushort MinLINQ()
+    //    {
 
-            return source.Min();
-        }
+    //        return source.Min();
+    //    }
 
-        [Benchmark]
-        public ushort MinLoop()
-        {
+    //    [Benchmark]
+    //    public ushort MinLoop()
+    //    {
 
-            ushort min = source[0];
-            for(int i = 1; i < source.Length; i++)
-            {
-                if (source[i] < min)
-                    min = source[i];
-            }
-            return min;
-        }
-    }
+    //        ushort min = source[0];
+    //        for(int i = 1; i < source.Length; i++)
+    //        {
+    //            if (source[i] < min)
+    //                min = source[i];
+    //        }
+    //        return min;
+    //    }
+    //}
 
     public class SumBenchmark
     {
-        int[] array = Enumerable.Repeat(0, 2097152).Select(i => new Random().Next(100)).ToArray();
+        internal float[] values;
 
-        [Benchmark]
-        public int Sum()
+        [Params(100_000)]
+        public int N;
+
+        [GlobalSetup]
+        public void Setup()
         {
-            var source = new ReadOnlySpan<int>(array);
-            int result = 0;
+            var rnd = new Random(42);
+            values = Enumerable.Range(0, N).Select(_ => (float)rnd.NextDouble()).ToArray();
+        }
 
-            for (int i = 0; i < source.Length; i++)
+        [Benchmark(Baseline = true)]
+        public float SumNaive()
+        {
+            var values = this.values;
+            float sum = 0;
+            for (int i = 0; i < values.Length; i++)
             {
-                result += source[i];
+                sum += values[i];
             }
-
-            return result;
+            return (float)sum;
         }
 
         [Benchmark]
-        public unsafe int SumUnrolled()
+        public float SumUsingLinq()
         {
-            var source = new ReadOnlySpan<int>(array);
-            int result = 0;
+            return values.Sum();
+        }
 
-            int i = 0;
-            int lastBlockIndex = source.Length - (source.Length % 4);
+        [Benchmark]
+        public float SumUsingSystemNumerics()
+        {
+            var values = this.values;
+            float sum = 0;
 
-            // Pin source so we can elide the bounds checks
-            fixed (int* pSource = source)
+            // How may elements can we sum using vectors?
+            int vectorizableLength = values.Length - values.Length % Vector<float>.Count;
+
+            // Create vectors from the values and sum them into a temporary vector
+            var tempVector = Vector<float>.Zero;
+            var valuesSpan = values.AsSpan();
+            for (int i = 0; i < vectorizableLength; i += Vector<float>.Count)
             {
-                while (i < lastBlockIndex)
-                {
-                    result += pSource[i + 0];
-                    result += pSource[i + 1];
-                    result += pSource[i + 2];
-                    result += pSource[i + 3];
+                tempVector += new Vector<float>(valuesSpan.Slice(i, Vector<float>.Count));
+            }
 
-                    i += 4;
+            // Sum the elements in the ttemporary vector
+            for (int iVector = 0; iVector < Vector<float>.Count; iVector++)
+            {
+                sum += tempVector[iVector];
+            }
+
+            // Handle remaining elements
+            for (int i = vectorizableLength; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+
+            return sum;
+        }
+
+        [Benchmark]
+        public float SumUsingAvx()
+        {
+            var values = this.values;
+            float sum = 0;
+
+            if (Avx.IsSupported)
+            {
+                // How may elements can we sum using vectors?
+                int vectorizableLength = values.Length - values.Length % Vector256<float>.Count;
+
+                // Create vectors from the values and sum them into a temporary vector
+                var tempVector = Vector256<float>.Zero;
+                unsafe
+                {
+                    fixed (float* valuesPtr = values)
+                    {
+                        for (int i = 0; i < vectorizableLength; i += Vector256<float>.Count)
+                        {
+                            var valuesVector = Avx.LoadVector256(valuesPtr + i);
+                            tempVector = Avx.Add(tempVector, valuesVector);
+                        }
+                    }
                 }
 
-                while (i < source.Length)
+                // Sum the elements in the ttemporary vector
+                for (int iVector = 0; iVector < Vector256<float>.Count; iVector++)
                 {
-                    result += pSource[i];
-                    i += 1;
+                    sum += tempVector.GetElement(iVector);
                 }
-            }
 
-            return result;
-        }
-
-        [Benchmark]
-        public int SumVectorT()
-        {
-            var source = new ReadOnlySpan<int>(array);
-            int result = 0;
-
-            Vector<int> vresult = Vector<int>.Zero;
-
-            int i = 0;
-            int lastBlockIndex = source.Length - (source.Length % Vector<int>.Count);
-
-            while (i < lastBlockIndex)
-            {
-                vresult += new Vector<int>(source.Slice(i));
-                i += Vector<int>.Count;
-            }
-
-            for (int n = 0; n < Vector<int>.Count; n++)
-            {
-                result += vresult[n];
-            }
-
-            while (i < source.Length)
-            {
-                result += source[i];
-                i += 1;
-            }
-
-            return result;
-        }
-
-        [Benchmark]
-        public int SumVectorized()
-        {
-            var source = new ReadOnlySpan<int>(array);
-            if (Avx2.IsSupported)
-            {
-                return SumVectorizedAvx2(source);
-            }
-            if (Sse2.IsSupported)
-            {
-                return SumVectorizedSse2(source);
+                // Handle remaining elements
+                for (int i = vectorizableLength; i < values.Length; i++)
+                {
+                    sum += values[i];
+                }
             }
             else
             {
-                return SumVectorT();
+                // non-AVX capable machines
+                for (int i = 0; i < values.Length; i++)
+                {
+                    sum += values[i];
+                }
             }
+
+            return sum;
         }
 
-        private unsafe int SumVectorizedSse2(ReadOnlySpan<int> source)
+        [Benchmark]
+        public float SumUsingAvxAlignedPipelined()
         {
-            int result;
+            var values = this.values;
+            float sum = 0;
 
-            fixed (int* pSource = source)
+            if (Avx.IsSupported)
             {
-                Vector128<int> vresult = Vector128<int>.Zero;
-
-                int i = 0;
-                int lastBlockIndex = source.Length - (source.Length % 4);
-
-                while (i < lastBlockIndex)
+                unsafe
                 {
-                    vresult = Sse2.Add(vresult, Sse2.LoadVector128(pSource + i));
-                    i += 4;
+                    fixed (float* valuesPtr = values)
+                    {
+                        // Determine how many elements we need to sum sequential to reach 256 bit alignment
+                        const int ElementsPerByte = sizeof(float) / sizeof(byte);
+                        var alignmentOffset = (long)(uint)(-(int)valuesPtr / ElementsPerByte) & (Vector256<float>.Count - 1);
+
+                        // handle first values sequentially until we hit the 256bit alignment boundary
+                        for (long i = 0; i < alignmentOffset; i++)
+                        {
+                            sum += *(valuesPtr + i);
+                        }
+
+                        var remainingLength = values.Length - alignmentOffset;
+
+                        var vectorizableLength = values.Length - remainingLength % (long)Vector256<float>.Count;
+
+                        // handle batches of 4 vectors for pipelining benefits
+                        var pipelineVectorizableLength = values.Length - remainingLength % (4 * (long)Vector256<float>.Count);
+
+                        var tempVector1 = Vector256<float>.Zero;
+                        var tempVector2 = Vector256<float>.Zero;
+                        var tempVector3 = Vector256<float>.Zero;
+                        var tempVector4 = Vector256<float>.Zero;
+                        for (long i = alignmentOffset; i < pipelineVectorizableLength; i += 4 * (long)Vector256<float>.Count)
+                        {
+                            var valuesVector1 = Avx.LoadAlignedVector256(valuesPtr + i);
+                            var valuesVector2 = Avx.LoadAlignedVector256(valuesPtr + i + Vector256<float>.Count);
+                            var valuesVector3 = Avx.LoadAlignedVector256(valuesPtr + i + 2 * Vector256<float>.Count);
+                            var valuesVector4 = Avx.LoadAlignedVector256(valuesPtr + i + 3 * Vector256<float>.Count);
+                            tempVector1 = Avx.Add(tempVector1, valuesVector1);
+                            tempVector2 = Avx.Add(tempVector2, valuesVector2);
+                            tempVector3 = Avx.Add(tempVector3, valuesVector3);
+                            tempVector4 = Avx.Add(tempVector4, valuesVector4);
+                        }
+
+                        var tempVector = Avx.Add(Avx.Add(Avx.Add(tempVector1, tempVector2), tempVector3), tempVector4);
+
+                        // handle remaining vectors
+                        for (long i = pipelineVectorizableLength; i < vectorizableLength; i += Vector256<float>.Count)
+                        {
+                            var valuesVector = Avx.LoadAlignedVector256(valuesPtr + i);
+                            tempVector = Avx.Add(tempVector, valuesVector);
+                        }
+
+                        // sum the temporary vector
+                        for (int iVector = 0; iVector < Vector256<float>.Count; iVector++)
+                        {
+                            sum += tempVector.GetElement(iVector);
+                        }
+
+                        // handle remaining items
+                        for (int i = (int)vectorizableLength; i < values.Length; i++)
+                        {
+                            sum += values[i];
+                        }
+                    }
                 }
-
-                if (Ssse3.IsSupported)
+            }
+            else
+            {
+                // non-AVX capable machines
+                for (int i = 0; i < values.Length; i++)
                 {
-                    vresult = Ssse3.HorizontalAdd(vresult, vresult);
-                    vresult = Ssse3.HorizontalAdd(vresult, vresult);
-                }
-                else
-                {
-                    vresult = Sse2.Add(vresult, Sse2.Shuffle(vresult, 0x4E));
-                    vresult = Sse2.Add(vresult, Sse2.Shuffle(vresult, 0xB1));
-                }
-                result = vresult.ToScalar();
-
-                while (i < source.Length)
-                {
-                    result += pSource[i];
-                    i += 1;
+                    sum += values[i];
                 }
             }
 
-            return result;
-        }
-
-        private unsafe int SumVectorizedAvx2(ReadOnlySpan<int> source)
-        {
-            int result;
-
-            fixed (int* pSource = source)
-            {
-                Vector256<int> vresult = Vector256<int>.Zero;
-
-                int i = 0;
-                int lastBlockIndex = source.Length - (source.Length % 8);
-
-                while (i < lastBlockIndex)
-                {
-                    vresult = Avx2.Add(vresult, Avx.LoadAlignedVector256(pSource + i));
-                    i += 8;
-                }
-
-                vresult = Avx2.HorizontalAdd(vresult, vresult);
-                vresult = Avx2.HorizontalAdd(vresult, vresult);
-                result = vresult.ToScalar();
-
-                while (i < source.Length)
-                {
-                    result += pSource[i];
-                    i += 1;
-                }
-            }
-
-            return result;
+            return sum;
         }
     }   
 }
